008317d141df062afc436ef58e198d0e2293c330
mary ann <maryannbernardo212@gmail.com>
2022-05-26 15:51:55 +0800

    first excercise 1.10

---
+++ excercise-variables_concatenation.py
@@ -0,0 +1,9 @@
+character_job = "Pirate"
+character_ride = "Ship"
+character_souvenir = "Gold"
+character_pet = "Parrot"
+
+print ("There was once a " + character_job + " who loved adventure.")
+print ("The " + character_job + " would take her " + character_ride + " to unknown places.")
+print ("She brings home a lot of " + character_souvenir + ".")
+print ("Then she goes home to her pet " + character_pet + ", Chuckles.")
