f0bb7b4f3bde3fd71d7192e9bc5ddeb129299ffb
mary ann <maryannbernardo212@gmail.com>
2022-05-27 02:59:02 +0800

    Excercise 3.4

---
+++ class_variable.py
@@ -0,0 +1,18 @@
+class customers:
+    greeting = "Welcome to the Cofee Palace!"
+    
+c_1 = customers ()
+c_1.name = "Samirah"
+c_1.beverage = "Iced Cafe Latte"
+c_1.food = "Cinnamon Roll"
+c_1.total = 225
+
+c_2 = customers
+c_2.name = "Jeremy"
+c_2.beverage = "Caramel Macchiato"
+c_2.food = "Glazed Doughnut"
+c_2.total = 230
+
+print(customers.greeting)
+print("c_1.beverage")
+print("c_2.food")
