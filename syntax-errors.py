07b67323b01c98732fe0782f2ff06a9cce64656e
mary ann <maryannbernardo212@gmail.com>
2022-05-27 00:35:52 +0800

    Excercise 1.14

---
+++ syntax-errors.py
@@ -0,0 +1,11 @@
+#this for number one line
+print ("Roses are Red, Violets are Blue")
+
+#this is for number two line
+if 10 > 7:
+    print ("Ten is greater than Seven!")
+if 16 < 42:
+    print ("Sixteen is less than Forty-two!")
+
+#this is for number three line
+print("A long time ago, in a Galaxy far, far away...")
