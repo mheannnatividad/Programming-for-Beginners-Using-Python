1d1396da6f217d738c4b3b1c552e02055b27cf94
mary ann <maryannbernardo212@gmail.com>
2022-05-27 16:06:34 +0800

    Last excercise

---
+++ Loops.py
@@ -0,0 +1,15 @@
+#For Loops
+furniture = ["table", "chair", "cabinet", "desk", "couch"]
+for x in furniture:
+    if x == "cabinet":
+        continue
+    print(x)
+
+#While Loops
+i = 1
+while i < 15:
+    print (i)
+    i += 1
+else:
+    print("i is no longer less than 15.")
+
